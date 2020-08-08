c7d48d2b0f96ee81114a33693a94cb5ba5e30594
Bugra Aydingoz <bugraaydingoz@gmail.com>
2020-08-08 22:53:36 +0200

    Put icons

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,17 +3,59 @@ import React from 'react'
 // Components
 import { ReactComponent as MenuIcon } from '../icons/menu.svg'
 import { ReactComponent as Logo } from '../icons/logo.svg'
-import { ReactComponent as MessageActive } from '../icons/message-active.svg'
+import { ReactComponent as MessageActiveIcon } from '../icons/message-active.svg'
+import { ReactComponent as ContactIcon } from '../icons/contact.svg'
+import { ReactComponent as UserIcon } from '../icons/user.svg'
+import { ReactComponent as LogoutIcon } from '../icons/logout.svg'
 
 // Styles
-import { HeaderContainer } from './styles'
+import { HeaderContainer, ActionsContainer, ActionContainer } from './styles'
 
 export function Header() {
+  const isMobile = window.innerWidth <= 768
   return (
     <HeaderContainer>
-      <MenuIcon />
-      <Logo />
-      <MessageActive />
+      {isMobile ? <HeaderMobile /> : <HeaderDesktop />}
     </HeaderContainer>
+  )
+}
+
+function HeaderMobile() {
+  return (
+    <>
+      <MenuIcon />
+      <Logo style={{ width: '100' }} />
+      <MessageActiveIcon />
+    </>
+  )
+}
+
+function HeaderDesktop() {
+  return (
+    <>
+      {/* Left buttons */}
+      <ActionsContainer>
+        <ActionContainer>
+          <MenuIcon />
+        </ActionContainer>
+        <Logo />
+      </ActionsContainer>
+
+      {/* Right buttons */}
+      <ActionsContainer>
+        <ActionContainer>
+          <ContactIcon />
+        </ActionContainer>
+        <ActionContainer>
+          <MessageActiveIcon />
+        </ActionContainer>
+        <ActionContainer>
+          <UserIcon />
+        </ActionContainer>
+        <ActionContainer>
+          <LogoutIcon />
+        </ActionContainer>
+      </ActionsContainer>
+    </>
   )
 }
--- src/components/header/styles.ts
+++ src/components/header/styles.ts
@@ -3,5 +3,26 @@ import styled from 'styled-components'
 export const HeaderContainer = styled.header`
   display: flex;
   justify-content: space-between;
+  align-items: center;
   margin: var(--space-sm) var(--space-md);
+
+  @media (min-width: 768px) {
+    margin: var(--space-md) var(--space-xl);
+  }
+`
+
+export const ActionsContainer = styled.div`
+  display: flex;
+`
+
+export const ActionContainer = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  width: 32px;
+  margin-right: var(--space-md);
+
+  &:last-child {
+    margin-right: 0;
+  }
 `
